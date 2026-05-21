e361536cca80506ee64bc6fbce368b27e0087eb5
Rafaël HOFF <hoff.rafael06@gmail.com>
2026-05-21 12:02:31 +0200

    ajustement pid

--- TraitementImg.py
+++ TraitementImg.py
@@ -33,7 +33,7 @@ def main():
     
     # Initialisation du PID (Coefficients à ajuster lors de tes tests !)
     # Règle d'abord Kp (ex: 0.4), laisse Ki à 0, et mets un poil de Kd (ex: 0.05)
-    pid = PID(kp=0.08, ki=0.0, kd=0.05)
+    pid = PID(kp=0.12, ki=0.0, kd=0.07)
     
     # Vitesse de croisière du robot (sur 100)
     VITESSE_MAX = 35
